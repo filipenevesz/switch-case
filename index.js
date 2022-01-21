0064e02d9fd02a634b18d5dfc88f8fd9f9ac0afb
Filipe <fedzn11@gmail.com>
2022-01-21 16:12:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+function getDiaSemanaTexto (diaSemana) {
+    let diaSemanaTexto;
+    switch(diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+         case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+         case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+         case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+         case 6:
+            diaSemanaTexto = 'Sabado';
+            return diaSemanaTexto;
+         default:
+            diaSemana = '';                       
+    }
+
+    
+}
+
+const data = new Date('1981-04-02 00:00:00')
+let diaSemana = data.getDay();
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+console.log(diaSemana, diaSemanaTexto);
